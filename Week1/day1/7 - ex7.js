f3f1cfcdabdc3436e690b4a195c3c1f32e5b737d
Tatiana Panferova <tatiana.panferova@gmail.com>
2019-09-12 08:50:27 +0000

    Update 7 - ex7.js

--- Week1/day1/7 - ex7.js
+++ Week1/day1/7 - ex7.js
@@ -1,14 +1,24 @@
 //7. Cat and Mouse
 
+// You will be given a string featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
+
+// You need to find out if the cat can catch the mouse from it's current position. The cat can jump three characters.
+
+
 function isCaught(str) {
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === 'C' && str[i + 3] === 'm') {
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === 'C' && 
+        (str[i + 3] === 'm' || str[i + 2] === 'm' || str[i + 1] === 'm')) {
             return true;
-        } 
+        }
     }
     return false;
 }
 
+//ES6
+
+const isCaughtES6 = str => str.indexOf('C') + 3 >= str.indexOf('m') ? true : false;
+
 console.log(isCaught('C.....m')); // => false
 console.log(isCaught('C..m'));// => true
-console.log(isCaught('..C..m')); // => true
+console.log(isCaughtES6('..C..m')); // => true
