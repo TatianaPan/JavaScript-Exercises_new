581e0e6e8cc154e5edbe9f9a89fb78c4475162dd
Tatiana Panferova <tatiana.panferova@gmail.com>
2019-09-12 08:57:09 +0000

    Update 13 - ex13.js

--- Week1/day1/13 - ex13.js
+++ Week1/day1/13 - ex13.js
@@ -1,17 +1,13 @@
 // 13.Confirm the Ending
 
 function confirmEnding(str, target) {
-    let num = target.length;
-    let subStr = str.substr(-num);
-    if (subStr === target) {
-        return true;
-    } else {
-        return false;
-    }
+    let targetLength = target.length;
+    let newStr = str.slice(-targetLength);
+    return newStr === target;
 }
 
-// let str = "Open sesame";
-// let target = "same";
-// const confirmEnding = (str, target) => {str.substr(-target.length) === target ? true : false};
+//ES6
+
+const confirmEndingES6 = (str, target) => str.slice(-target.length) === target;
 
 console.log(confirmEnding("Open sesame", "same")); // true
