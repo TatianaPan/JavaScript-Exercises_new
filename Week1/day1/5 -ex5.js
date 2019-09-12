1599253c13dd25d2dddd63b4d8e7f1746345ea64
Tatiana Panferova <tatiana.panferova@gmail.com>
2019-09-12 08:49:02 +0000

    Update 5 -ex5.js

--- Week1/day1/5 -ex5.js
+++ Week1/day1/5 -ex5.js
@@ -1,11 +1,18 @@
 // 5. Convert Number to Reversed Array of Digits
 
-// function convert(numb) {
-//     let arr = numb.toString().split('').sort().reverse();
-//     return arr;
-// }
 
-const convert = numb => numb.toString().split('').sort().reverse();
+function convert(num) {
+    const arr = num.toString().split('');
+    
+    const result = arr.map(function(x) {
+        return parseInt(x, 10)
+    }).sort().reverse();
+    return result;
+}
+
+//ES6 
+
+const convertES6 = num => num.toString().split('').map(x => parseInt(x, 10)).sort().reverse();
 
 console.log(convert(429563)) // => [9, 6, 5, 4, 3, 2]
-console.log(convert(324)) // => [4, 3, 2]
+console.log(convertES6(324)) // => [4, 3, 2]
